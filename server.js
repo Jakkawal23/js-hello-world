b11e26089502838c01338caf20b549280fab37c0
Jakkawal_Galaxy <jakkawal.phooluemsai@gmail.com>
2022-08-27 04:24:24 +0000

    Update message

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดีเมืองพะเยาจ้า</h1>')
+  return res.send('<h1>Hello Jakkawal Phooluemsai</h1>')
+  return res.send('<h1>สวัสดี ด็อกเตอร์ว่าที่ร้อยตรีจักรวาล ภูเลื่อมใส</h1>')
 })
 
 const PORT = process.env.PORT || 3000
