79aff3cf509d0bec263f9d0eb0f8a3e87cc2688e
Jakkawal_Galaxy <jakkawal.phooluemsai@gmail.com>
2022-08-28 04:11:07 +0000

    edit message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>คืนนี้ไป BM กันไหม</h1>')
+  return res.send('<h1>คืนนี้นอนดีกว่า</h1>')
 })
 
 const PORT = process.env.PORT || 3000
