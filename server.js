764018473000b577171d5eb07dd60f7cf926ac1c
Jakkawal_Galaxy <jakkawal.phooluemsai@gmail.com>
2022-08-28 07:48:42 +0000

    Edit jakkawal branch

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   return res.send('<h1>In branch develop</h1>')
 })
 
+app.get('/jakkawal', (req, res) => {
+  return res.send('<h1>In branch feature jakkawal</h1>')
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.info('Server is ready at', PORT)
