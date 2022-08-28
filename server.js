68969159e5888401c867b2b59b3ee9fe3c5c94b6
MaxGaZ1nE <maxja65as@gmail.com>
2022-08-28 07:53:16 +0000

    max

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   return res.send('<h1>In branch develop</h1>')
 })
 
+app.get('/max', (req, res) => {
+  return res.send('<h1>ตองเอี่ยว</h1>')
+})
+
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.info('Server is ready at', PORT)
